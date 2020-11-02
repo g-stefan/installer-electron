00647f1606019dafa6245f56bd0004d36d1095cd
Grigore Stefan <g_stefan@yahoo.com>
2020-11-02 02:29:32 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -3,4 +3,6 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
+set PRODUCT_NAME=installer-electron
 set PRODUCT_VERSION=10.1.5
+set PRODUCT_BASE=electron
--- build.make.cmd
+++ build.make.cmd
@@ -3,9 +3,9 @@ rem Public domain
 rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo -^> make electron
-
 call build.config.cmd
+
+echo -^> make %PRODUCT_NAME%
 
 if exist release\ rmdir /Q /S release
 if exist build\ rmdir /Q /S build
