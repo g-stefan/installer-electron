6af4972b69136358e8a11e34df73418ceac6e622
Grigore Stefan <g_stefan@yahoo.com>
2021-02-12 01:47:51 +0200

    update

--- build.config.cmd
+++ build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-electron
-set PRODUCT_VERSION=10.1.5
+set PRODUCT_VERSION=11.2.3
 set PRODUCT_BASE=electron
